d3654e5779f4065ad51d05e5f7dd3a8ce8908021
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2021-10-07 14:15:27 -0700

    added redux/styled components. created API rout for products

---
+++ src/components/AppStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AppBody = styled.div`
+  min-height: 100vh;
+  /* display: flex;
+  flex-flow: column wrap; */
+background: coral;
+
+`;
+
+export default AppBody;
